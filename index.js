487f730ff43c79c55dae45ae63fd5112edd77ace
Bigetion <adysuprianto@yahoo.co.id>
2021-07-08 21:55:31 +0800

    Update Version 1.9.1

--- index.js
+++ index.js
@@ -324,6 +324,8 @@ function generateTailwindCss(options = {}) {
 		? generateAccessibility(configOptions)
 		: "";
 
+  cssString = cssString.replace(/\s\s+/g, ' ');
+
 	const head = document.head || document.getElementsByTagName("head")[0];
 	const style = document.createElement("style");
 	head.appendChild(style);
