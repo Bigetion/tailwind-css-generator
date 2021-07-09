97008a952e0970401f1f0d08881169f72fe0017a
Bigetion <adysuprianto@yahoo.co.id>
2021-07-09 13:21:16 +0800

    Update version 1.9.6

--- generators/boxShadow.js
+++ generators/boxShadow.js
@@ -5,17 +5,30 @@ export default function generateBoxShadow(configOptions = {}) {
 
   const prefix = `${globalPrefix}shadow`;
 
-  const { boxShadow: propertyOptions = {} } = theme;
+  const { colors, boxShadow: propertyOptions = {} } = theme;
 
   const responsiveCssString = generateCssString(
-    ({ pseudoClass, getCssByOptions }) => {
-      const cssString = getCssByOptions(propertyOptions, (keyTmp, value) => {
+    ({ pseudoClass, getCssByOptions, getCssByColors }) => {
+      let cssString = getCssByOptions(propertyOptions, (keyTmp, value) => {
         const key = keyTmp !== "default" ? `-${keyTmp}` : "";
         return `
           ${pseudoClass(`${prefix}${key}`, variants.boxShadow)} {
             box-shadow: ${value};
           }
         `;
+      });
+      cssString += getCssByColors(colors, (...colorArgs) => {
+        const key = colorArgs[0];
+        const rgbValue = colorArgs[2];
+        let str = "";
+        if (rgbValue) {
+          str += `
+            ${pseudoClass(`${prefix}-outline-${key}`)} {
+              box-shadow: 0 0 0 3px rgba(${rgbValue}, 0.5) !important;
+            }
+          `;
+        }
+        return str;
       });
       return cssString;
     },
